691b068afa2a0a3638cd4b9f2c2eac5c55951ac3
p-barski <patryk.kubarski@protonmail.com>
2026-02-01 13:23:36 +0100

    Added ScaleFactor to SpriteData

--- src/RAMpocalypse.Server/Game/SpriteData.cs
+++ src/RAMpocalypse.Server/Game/SpriteData.cs
@@ -1,8 +1,9 @@
 namespace RAMpocalypse.Server.Game;
 
-public class SpriteData(string url, int width = 64, int height = 32)
+public class SpriteData(string url, int width = 64, int height = 32, int scaleFactor = 8)
 {
     public string URL { get; init; } = url;
     public int Width { get; init; } = width;
     public int Height { get; init; } = height;
+    public int ScaleFactor { get; init; } = scaleFactor;
 }
